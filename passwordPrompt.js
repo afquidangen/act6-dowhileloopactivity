666dc97359c41d0e4df744e76265cc87639c725c
afquidangen <alyssafayequidangen@gmail.com>
2024-10-09 20:18:09 +0800

    Do While Loop Activity

--- passwordPrompt.js
+++ passwordPrompt.js
@@ -2,10 +2,11 @@ function getPassword () {
     let password = prompt("Enter your password: ");
     let myPassword = ("ispscmain@bscs2a");
     do {
-        alert("Incorrect. Try again.");
+        alert("Incorrect, try again.");
         password = prompt("Enter your password: ");
-} while (password != myPassword ); 
- if (password == myPassword) { 
-    alert("Access granted."); 
+} while (password != myPassword);
+if (password == myPassword) {
+    alert("Access granted!");
 }
 }
+ 
